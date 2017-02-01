73c8039bc4ed4e29128b4583483dc35f8b1b7c34
Jagadesh Seeram <seeram.jagadesh@gmail.com>
2017-02-01 18:36:57 +0530

    Added the changes

--- app/src/main/java/com/biglynx/fulfiller/adapter/FufillerDetails_Adapter.java
+++ app/src/main/java/com/biglynx/fulfiller/adapter/FufillerDetails_Adapter.java
@@ -2,8 +2,6 @@ package com.biglynx.fulfiller.adapter;
 
 
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.content.res.XmlResourceParser;
 import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,7 +12,6 @@ import android.widget.TextView;
 
 import com.biglynx.fulfiller.R;
 import com.biglynx.fulfiller.models.FulfillersDTO;
-import com.biglynx.fulfiller.models.RecentSearch;
 import com.biglynx.fulfiller.ui.FulfillmentDetails;
 
 import java.util.List;
@@ -24,15 +21,16 @@ public class FufillerDetails_Adapter extends BaseAdapter {
     Context Context;
     List<FulfillersDTO> fulfillersDTOList;
     LayoutInflater inflater;
-    public FufillerDetails_Adapter(Context mContext, List<FulfillersDTO> fulfillersDTOLists){
-        Context=mContext;
-        fulfillersDTOList=fulfillersDTOLists;
+
+    public FufillerDetails_Adapter(Context mContext, List<FulfillersDTO> fulfillersDTOLists) {
+        Context = mContext;
+        fulfillersDTOList = fulfillersDTOLists;
         inflater = LayoutInflater.from(this.Context);
     }
 
     @Override
     public int getCount() {
-        Log.d("recent search count",""+fulfillersDTOList.size());
+        Log.d("recent search count", "" + fulfillersDTOList.size());
         return fulfillersDTOList.size();
 
     }
@@ -53,21 +51,22 @@ public class FufillerDetails_Adapter extends BaseAdapter {
 
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.recent_search, parent, false);
+            convertView.setBackgroundColor(Color.parseColor("#353944"));
             mViewHolder = new MyViewHolder(convertView);
             convertView.setTag(mViewHolder);
         } else {
             mViewHolder = (MyViewHolder) convertView.getTag();
         }
-        if(position == 0){
+        if (position == 0) {
             mViewHolder.place_tv.setSelected(true);
         }
-        mViewHolder.place_tv.setText("FULFILMENT "+(position+1));
+        mViewHolder.place_tv.setText("FULFILMENT " + (position + 1));
         mViewHolder.place_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FulfillmentDetails fulfillmentDetailsFrag = (FulfillmentDetails) Context;
-                fulfillmentDetailsFrag.fillfromData(position,true);
-                for(int i = 0 ;i < parent.getChildCount();i++){
+                fulfillmentDetailsFrag.fillfromData(position, true);
+                for (int i = 0; i < parent.getChildCount(); i++) {
                     parent.getChildAt(i).findViewById(R.id.place_tv).setSelected(false);
                 }
                 v.setSelected(true);
