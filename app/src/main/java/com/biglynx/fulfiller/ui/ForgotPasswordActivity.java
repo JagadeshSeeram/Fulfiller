7541e9bf29118de42d438187d892b6cd59411bf2
sneha2112 <snehithachinnapally.21@gmail.com>
2017-04-06 10:56:27 +0530

    forgot password implemented

---
+++ app/src/main/java/com/biglynx/fulfiller/ui/ForgotPasswordActivity.java
@@ -0,0 +1,95 @@
+package com.biglynx.fulfiller.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.biglynx.fulfiller.R;
+import com.biglynx.fulfiller.network.FullFillerApiWrapper;
+import com.biglynx.fulfiller.utils.AppPreferences;
+import com.biglynx.fulfiller.utils.AppUtil;
+import com.biglynx.fulfiller.utils.Common;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ForgotPasswordActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView icon_back, title_tv, reset_password_tv;
+    private EditText email_et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        initViews();
+    }
+
+    private void initViews() {
+        icon_back = (TextView) findViewById(R.id.icon_back);
+        title_tv = (TextView) findViewById(R.id.companyname_tv);
+        reset_password_tv = (TextView) findViewById(R.id.reset_password);
+        email_et = (EditText) findViewById(R.id.email_edit);
+        title_tv.setVisibility(View.GONE);
+        icon_back.setOnClickListener(this);
+        reset_password_tv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.icon_back:
+                finish();
+                break;
+            case R.id.reset_password:
+                if (!AppUtil.ifNotEmpty(email_et.getText().toString())) {
+                    AppUtil.toast(ForgotPasswordActivity.this, "Please enter your email");
+                    return;
+                }
+
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email_et.getText().toString().trim()).matches()) {
+                    AppUtil.toast(this, "Email is not valid!!");
+                    return;
+                }
+                callService();
+                break;
+        }
+    }
+
+    private void callService() {
+        Common.showDialog(ForgotPasswordActivity.this);
+        FullFillerApiWrapper apiWrapper = new FullFillerApiWrapper();
+        apiWrapper.resetPasswordCall(AppPreferences.getInstance(getApplicationContext()).getSignInResult() != null ?
+                        AppPreferences.getInstance(getApplicationContext()).getSignInResult().optString("AuthNToken") : "",
+                email_et.getText().toString().trim(), new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.isSuccessful()) {
+                            AppUtil.toast(getApplicationContext(), "Please check your mail...");
+                        }
+                        else {
+                            try {
+                                AppUtil.parseErrorMessage(getApplicationContext(), response.errorBody().string());
+                            } catch (IOException e) {
+                                AppUtil.toast(getApplicationContext(), getString(R.string.OOPS));
+                                e.printStackTrace();
+                            }
+                            AppUtil.CheckErrorCode(ForgotPasswordActivity.this, response.code());
+                        }
+                        Common.disMissDialog();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Common.disMissDialog();
+                        Log.e("ForGotPwd :: ","Forgot password error");
+                        t.printStackTrace();
+                    }
+                });
+    }
+}
